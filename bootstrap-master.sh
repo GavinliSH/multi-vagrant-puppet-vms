93101750d2942ceff94bb4610c8bb5a1fb054515
xiaoguangli <xiaoguang.li@capgemini.com>
2018-08-24 17:52:47 +0800

    first commit

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -8,28 +8,30 @@ then
 else
     # Install Puppet Master
     echo "Start install puppet master"
-    wget --no-check-certificate https://apt.puppetlabs.com/puppetlabs-release-trusty.deb && \
-    sudo dpkg -i puppetlabs-release-trusty.deb && \
-    sudo apt-get update -yq && sudo apt-get upgrade -yq && \
-    sudo apt-get install -yq puppetmaster
+    sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm && \
+    sed '22c enable=1' /etc/yum.repos.d/puppetlabs.repo && \
+    sudo yum -y install puppetserver && \
+    sudo puppet resource package puppet-server ensure=latest && \
+    sudo yum -y install puppet
+    sudo puppet resource package puppet ensure=latest
 
     # Configure /etc/hosts file
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.32.5    puppet.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.10   node01.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.20   node02.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
+    echo "192.168.32.10   wso2.example.com  wso2" | sudo tee --append /etc/hosts 2> /dev/null &&
 
     # Add optional alternate DNS names to /etc/puppet/puppet.conf
     sudo sed -i 's/.*\[main\].*/&\ndns_alt_names = puppet,puppet.example.com/' /etc/puppet/puppet.conf
 
     # Install some initial puppet modules on Puppet Master server
-    sudo puppet module install puppetlabs-ntp
-    sudo puppet module install garethr-docker
-    sudo puppet module install puppetlabs-git
-    sudo puppet module install puppetlabs-vcsrepo
-    sudo puppet module install garystafford-fig
+    sudo puppet module install puppetlabs-ntp && \
+    sudo puppet module install garethr-docker && \
+    sudo puppet module install puppetlabs-git && \
+    sudo puppet module install puppetlabs-vcsrepo && \
+    sudo puppet module install garystafford-fig &&
 
     # symlink manifest from Vagrant synced folder location
     ln -s /vagrant/site.pp /etc/puppet/manifests/site.pp
 fi
+sudo systemctl enable puppetmaster
--- bootstrap-node.sh
+++ bootstrap-node.sh
@@ -6,15 +6,14 @@ if ps aux | grep "puppet agent" | grep -v grep 2> /dev/null
 then
     echo "Puppet Agent is already installed. Moving on..."
 else
-    sudo apt-get install -yq puppet
+    sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm && \
+    sudo yum install -y puppet
 fi
 
 if cat /etc/crontab | grep puppet 2> /dev/null
 then
     echo "Puppet Agent is already configured. Exiting..."
 else
-    sudo apt-get update -yq && sudo apt-get upgrade -yq
-
     sudo puppet resource cron puppet-agent ensure=present user=root minute=30 \
         command='/usr/bin/puppet agent --onetime --no-daemonize --splay'
 
@@ -24,11 +23,10 @@ else
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.32.5    puppet.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.10   node01.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.20   node02.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
+    echo "192.168.32.10   wso2.example.com  wso2" | sudo tee --append /etc/hosts 2> /dev/null
 
     # Add agent section to /etc/puppet/puppet.conf
-    echo "" && echo "[agent]\nserver=puppet" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
+    echo "" && echo "server=puppet" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
 
-    sudo puppet agent --enable
+    sudo systemctl start puppet
 fi
